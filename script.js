6742699edd77328f69b9e5df70c71adc2925859e
eLn86 <elnathan.erh@gmail.com>
2017-05-24 11:04:38 +0800

    updated code from Partner1

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript to be added
+window.onload = function(){
+  var button = document.getElementById("button-yellow");
+  button.addEventListener("click", function(event){
+    prompt('What is your email?')
+  });
+}
+//additional comment
